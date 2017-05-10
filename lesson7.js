4908548099bab134f5f4a845f16e7e9c0e9f6b9f
Sevada <Sevada.Poxosyan@mail.ru>
2017-05-10 16:33:18 +0400

    Add files via upload

---
+++ lesson7.js
@@ -0,0 +1,16 @@
+
+//------------------
+var multiplier = 9;
+for (var i = 0; i<=10; i++){
+    var result = multiplier * i;
+    console.log(multiplier + "*" +  i + '='  + result);
+    //--------------------------
+}
+var i = 0;
+while(i<10) {
+    console.log("The number is" + i);
+    i * 10;
+}
+
+
+
